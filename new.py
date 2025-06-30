312c988d477947355821b2437f3fa189165ea168
yichosparda <jasmine.yuan@education.nsw.gov.au>
2025-06-30 10:58:32 +1000

    first commit

---
+++ new.py
@@ -0,0 +1,17 @@
+import tkinter as tk 
+from PIL import Image, ImageTk
+def main():
+# Create the main window
+    root = tk.Tk()
+    root.title("Cat Viewer")
+    # Load the image (replace 'cat.png' with your image filename)
+    pil_image = Image.open("/Users/jasmine/Documents/School/py/palace.png")
+    # Convert to a Photolmage
+    tk_image = ImageTk.PhotoImage(pil_image)
+    # Create a label to hold the image and pack it into the window
+    label = tk.Label(root, image=tk_image)
+    label.pack(padx=10, pady=10)
+    # Start the GUI event loop
+    root.mainloop()
+if __name__ == "__main__":
+    main()
